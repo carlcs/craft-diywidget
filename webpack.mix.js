6c77488ededb786171416a1398192a051572e2da
carlcs <seelbach@gmx.de>
2020-08-12 13:34:46 +0200

    Move js and css files to resources dir

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,11 @@ const mix = require('laravel-mix');
 
 const paths = {
     src: '_examples/src',
-    dist: '_examples/diy-widget',
+    dist: '_examples/diy-widget/resources',
 };
 
 mix.js(paths.src+'/countdown.js', paths.dist)
-    .js(paths.src+'/search.js', paths.dist)
     .sass(paths.src+'/countdown.scss', paths.dist)
-    .sass(paths.src+'/photo.scss', paths.dist)
-    .sass(paths.src+'/search.scss', paths.dist)
-    .sass(paths.src+'/weekday-bear.scss', paths.dist);
+    .sass(paths.src+'/ratio.scss', paths.dist)
+    .js(paths.src+'/search.js', paths.dist)
+    .sass(paths.src+'/search.scss', paths.dist);
