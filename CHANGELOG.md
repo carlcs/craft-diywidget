c8cae9441374bf3c95a4fa180c728910f39efd7b
carlcs <seelbach@gmx.de>
2020-08-12 16:41:12 +0200

    Register template hooks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 ### Changed
 - All JavaScript and CSS files found in `config/diy-widget` are now loaded for CP requests, not just the ones matching template filenames.
+- It’s now possible to register template hooks by uploading PHP files to `config/diy-widget`. ([#9](https://github.com/carlcs/craft-diywidget/issues/9))
 - Widget titles are now displayed in Title Case.
 
 ## 2.0.2 - 2020-04-02
--- src/Plugin.php
+++ src/Plugin.php
@@ -6,6 +6,7 @@ use carlcs\diywidget\assets\WidgetsAsset;
 use carlcs\diywidget\services\Widgets;
 use Craft;
 use craft\events\RegisterComponentTypesEvent;
+use craft\helpers\FileHelper;
 use craft\services\Dashboard;
 use yii\base\Event;
 
@@ -37,6 +38,7 @@ class Plugin extends \craft\base\Plugin
 
         if (Craft::$app->getRequest()->getIsCpRequest()) {
             Craft::$app->getView()->registerAssetBundle(WidgetsAsset::class);
+            $this->_registerTemplateHooks();
 
             foreach ($widgets = $this->getWidgets()->getAllWidgets() as $widget) {
                 $this->_includeWidget($widget);
@@ -82,5 +84,24 @@ class Plugin extends \craft\base\Plugin
         ]);
 
         eval($code);
+    }
+
+    /**
+     * Registers template hooks.
+     */
+    private function _registerTemplateHooks()
+    {
+        $paths = FileHelper::findFiles(Craft::getAlias('@config/diy-widget'), [
+            'only' => ['*.php'],
+        ]);
+
+        foreach ($paths as $path) {
+            $hook = include($path);
+            if (is_callable($hook)) {
+                Craft::$app->view->hook('diy.' . pathinfo($path, PATHINFO_FILENAME), function(array &$context) use ($hook) {
+                    return $hook($context);
+                });
+            }
+        }
     }
 }
